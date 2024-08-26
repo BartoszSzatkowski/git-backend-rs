73bcbc88e68651d9bbaf4f4ec87b3bb57bb78f9e
Bartosz Szatkowski <szatek24@gmail.com>
2024-08-26 22:24:57 +0200

    feat: add pkt line and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fmt::Display;
+
+struct PktLine<'a>(&'a str);
+
+impl Display for PktLine<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0.is_empty() {
+            return write!(f, "0000");
+        }
+
+        if self.0.len() > 65516 {
+            panic!("Maximum payload for a PktLine exceeded");
+        }
+
+        write!(f, "{:04x}{}", self.0.len() + 4, self.0)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = PktLine("kiwi");
+    println!("{}", p);
 }
