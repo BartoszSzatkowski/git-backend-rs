2c7617e42e0d6968efc87a9d37793c91c6041ac2
Bartosz Szatkowski <szatek24@gmail.com>
2024-08-27 22:35:59 +0200

    feat: add gather refs for dumb server response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::fmt::Display;
+use std::fs;
+use std::path::Component;
+use std::path::Path;
+use walkdir::{DirEntry, WalkDir};
 
 struct PktLine<'a>(&'a str);
 
@@ -16,7 +20,55 @@ impl Display for PktLine<'_> {
     }
 }
 
+#[derive(Debug)]
+struct SimpleGitRef {
+    hash: String,
+    desc: String,
+}
+
+#[derive(Debug)]
+struct SimpleRefResponse {
+    refs: Vec<SimpleGitRef>,
+}
+
 fn main() {
-    let p = PktLine("kiwi");
-    println!("{}", p);
+    let mut res = Vec::new();
+    for entry in WalkDir::new("./.git/refs")
+        .min_depth(1)
+        .max_depth(2)
+        .into_iter()
+        .filter(is_file)
+    {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let hash = fs::read_to_string(&path).unwrap();
+        let mut new_path = path.components();
+        while let Some(v) = new_path.next() {
+            if let Component::Normal(v) = v {
+                if v.as_encoded_bytes() == b".git" {
+                    // Can be just `v == "bloo"`
+                    break;
+                }
+            }
+        }
+        let desc = new_path.as_path();
+
+        let git_ref = SimpleGitRef {
+            desc: desc.to_string_lossy().to_string(),
+            hash,
+        };
+        res.push(git_ref);
+    }
+
+    println!("{:?}", res);
+}
+
+fn is_file(dir_entry: &Result<walkdir::DirEntry, walkdir::Error>) -> bool {
+    let Ok(dir_entry) = dir_entry else {
+        return false;
+    };
+    let Ok(ent) = dir_entry.metadata() else {
+        return false;
+    };
+    ent.is_file()
 }
